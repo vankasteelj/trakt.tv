45e2851e9f9b44c58246f9a43a037719ee2c2347
vankasteelj <vankasteelj@gmail.com>
2017-07-20 20:29:03 +0200

    fix an "oauth optional" bug
    - _call() was sending "Bearer undefined" instead of sending no oauth header

--- trakt.js
+++ trakt.js
@@ -206,7 +206,7 @@ module.exports = class Trakt {
             body: (method.body ? Object.assign({}, method.body) : {})
         };
 
-        if (method.opts['auth']) req.headers['Authorization'] = `Bearer ${this._authentication.access_token}`;
+        if (method.opts['auth'] && this._authentication.access_token) req.headers['Authorization'] = `Bearer ${this._authentication.access_token}`;
 
         for (let k in params) {
             if (k in req.body) req.body[k] = params[k];
